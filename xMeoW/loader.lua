233845e203f09fa35026ecc50842c752114f1aba
MeoW:HUB <70427424+PomidorkaExlaL@users.noreply.github.com>
2023-10-08 19:13:58 +0500

    Delete xMeoW/loader.lua

--- xMeoW/loader.lua
+++
@@ -1,9 +0,0 @@
-if game.PlaceId == 12978425304 then
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/PomidorkaExlaL/MeoWHubMW/main/xMeoW/KeySystem')))()
-elseif game.PlaceId == 142823291 then
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/PomidorkaExlaL/MeoWHubMW/main/xMeoW/KeySystem')))()
-elseif game.PlaceId == 142823291 then
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/PomidorkaExlaL/MeoWHubMW/main/xMeoW/KeySystem')))()
-else
-	game.Players.LocalPlayer:Kick("This place is not supported.")
-end
