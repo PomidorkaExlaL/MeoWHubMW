89bf96cb5ae2aa171c3c8755fd89205cc2bf86ec
MeoW:HUB <70427424+PomidorkaExlaL@users.noreply.github.com>
2023-10-08 18:42:35 +0500

    Update loader.lua

--- xMeoW/loader.lua
+++ xMeoW/loader.lua
@@ -1,1 +1,9 @@
-if gameplaceid = ..
+if game.PlaceId == 12978425304 then
+	loadstring(game:HttpGet(('link')))()
+elseif game.PlaceId == 142823291 then
+	loadstring(game:HttpGet(('link')))()
+elseif game.PlaceId == 142823291 then
+	loadstring(game:HttpGet(('link')))()
+else
+	game.Players.LocalPlayer:Kick("This place is not supported.")
+end
